cf2e2ecaeb4446a9fa5dd164979d20401c173152
Quentin Barloy <quentin@les-tilleuls.coop>
2018-11-27 12:39:23 +0100

    Update default order

--- src/Entity/Car.php
+++ src/Entity/Car.php
@@ -10,6 +10,9 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource(
+ *     attributes={
+            "order": {"brand": "ASC"}
+ *     },
  *     itemOperations={
  *         "get",
  *         "patch": {"method": "PATCH"}
